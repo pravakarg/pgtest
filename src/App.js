8ff17e91be783653bdca9047b644918a11809a59
Pravakar Garnayak <pravakarg@outlook.com>
2022-12-19 19:24:47 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useSelector } from 'react-redux'
+
+import Settings from './Components/Settings'
+import Question from './Components/Question'
+import FinalScreen from './Components/FinalScreen'
+
+import './App.css'
+
+function App() {
+  const questions = useSelector((state) => state.questions)
+  const questionIndex = useSelector((state) => state.index)
+
+  let component
+
+  if (questions.length && questionIndex + 1 <= questions.length) {
+    component = <Question />
+  } else if (!questions.length) {
+    component = <Settings />
+  } else {
+    component = <FinalScreen />
+  }
+
+  return (
+    <div className="App">
+      <div className="app-container">{component}</div>
+    </div>
+  )
+}
+
+export default App
